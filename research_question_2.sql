ac56761c5f511769b2b1ef20b5e0b16be96f8bf9
Frantisek Sladek <frantisek.sladek.marketing@gmail.com>
2025-02-15 19:27:44 +0100

    research question n.2

---
+++ research_question_2.sql
@@ -0,0 +1,22 @@
+WITH year_limits AS (
+    SELECT 
+        MIN(year_from) AS min_year,
+        MAX(year_from) AS max_year
+    FROM t_frantisek_sladek_project_sql_primary_final
+    WHERE price_category LIKE '%Chléb%' OR price_category LIKE '%Mléko%'
+)
+SELECT
+	t.avg_price,
+	t.year_from,
+	t.price_category,
+	t.price_value,
+	t.price_unit,
+	ROUND(AVG(t.avg_wages), 2) AS annual_avg_wages,
+	ROUND(AVG(t.avg_wages) / AVG(t.avg_price), 2) AS pieces_per_wage
+FROM t_frantisek_sladek_project_sql_primary_final AS t
+JOIN year_limits AS yl
+	ON t.year_from=yl.min_year OR t.year_from=yl.max_year
+WHERE price_category LIKE '%Chléb%' OR 
+      price_category LIKE '%Mléko%'
+GROUP BY price_category, year_from, t.avg_price, t.price_value, t.price_unit;
+
