bc3fa1cdd11a9f2d2b6b1bba4e0656fd94fa8e02
Frantisek Sladek <frantisek.sladek.marketing@gmail.com>
2025-02-15 20:32:02 +0100

    research_question n.3

---
+++ research_question_3.sql
@@ -0,0 +1,30 @@
+WITH year_limits AS (
+    SELECT
+        MIN(year_from) AS min_year,
+        MAX(year_from) AS max_year
+    FROM t_frantisek_sladek_project_sql_primary_final
+)   
+SELECT
+	t.avg_price,
+	t.year_from,
+	t.price_category,
+	t.price_value,
+	t.price_unit,
+	yl.max_year - yl.min_year AS year_difference,
+	(t.avg_price - (
+        SELECT avg_price
+        FROM t_frantisek_sladek_project_sql_primary_final
+        WHERE price_category = t.price_category
+          AND year_from = yl.min_year
+        LIMIT 1
+    )) / (
+        SELECT avg_price
+        FROM t_frantisek_sladek_project_sql_primary_final
+        WHERE price_category = t.price_category
+          AND year_from = yl.min_year
+        LIMIT 1
+    ) * 100 AS percentage_growth
+FROM t_frantisek_sladek_project_sql_primary_final AS t
+JOIN year_limits AS yl
+	ON t.year_from=yl.min_year OR t.year_from=yl.max_year
+GROUP BY price_category, year_from, t.avg_price, t.price_value, t.price_unit, yl.min_year,yl.max_year;
