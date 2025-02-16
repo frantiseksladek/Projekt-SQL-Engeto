5f6070a601a7cb2e4255ee4082353582e512c088
Frantisek Sladek <frantisek.sladek.marketing@gmail.com>
2025-02-16 08:17:52 +0100

    research question n.4

--- research_question_4.sql
+++ research_question_4.sql
@@ -1,30 +1,37 @@
-WITH year_limits AS (
+WITH difference AS (
     SELECT
-        MIN(year_from) AS min_year,
-        MAX(year_from) AS max_year
-    FROM t_frantisek_sladek_project_sql_primary_final
-)   
+    	year_from,
+        t.avg_price - (
+            SELECT avg_price
+            FROM t_frantisek_sladek_project_sql_primary_final AS t_prev
+            WHERE t_prev.price_category = t.price_category
+              AND t_prev.year_from = t.year_from - 1
+            LIMIT 1
+        ) AS annual_price_difference,
+        t.avg_wages  - (
+            SELECT avg_wages
+            FROM t_frantisek_sladek_project_sql_primary_final AS t_prev
+            WHERE t_prev.industry_category = t.industry_category
+              AND t_prev.year_from = t.year_from - 1
+            LIMIT 1
+        ) AS annual_wage_difference
+    FROM t_frantisek_sladek_project_sql_primary_final AS t
+)
 SELECT
-	t.avg_price,
-	t.year_from,
-	t.price_category,
-	t.price_value,
-	t.price_unit,
-	yl.max_year - yl.min_year AS year_difference,
-	(t.avg_price - (
-        SELECT avg_price
-        FROM t_frantisek_sladek_project_sql_primary_final
-        WHERE price_category = t.price_category
-          AND year_from = yl.min_year
-        LIMIT 1
-    )) / (
-        SELECT avg_price
-        FROM t_frantisek_sladek_project_sql_primary_final
-        WHERE price_category = t.price_category
-          AND year_from = t.year_from -1
-        LIMIT 1
-    ) * 100 AS percentage_growth
+    t.avg_price,
+    t.year_from,
+    t.price_category,
+    t.price_value,
+    t.price_unit,
+    df.annual_price_difference,
+	ROUND((t.avg_price- annual_price_difference)/t.avg_price, 2) AS percentage_price_difference,
+    df.annual_wage_difference,
+    ROUND((t.avg_wages- annual_wage_difference)/t.avg_wages, 2) AS percentage_wage_difference
 FROM t_frantisek_sladek_project_sql_primary_final AS t
-JOIN year_limits AS yl
-	ON t.year_from=yl.min_year OR t.year_from=yl.max_year
-GROUP BY price_category, year_from, t.avg_price, t.price_value, t.price_unit, yl.min_year,yl.max_year;
+JOIN  difference AS df
+    ON df.year_from = t.year_from
+GROUP BY t.price_category, t.year_from, t.avg_price, t.price_value, t.price_unit
+HAVING percentage_price_difference  IS NOT NULL 
+	AND percentage_wage_difference IS NOT NULL 
+	AND (percentage_price_difference/percentage_wage_difference)>1.1;
+
